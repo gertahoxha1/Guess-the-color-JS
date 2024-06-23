96ea677534912d28d2713bd7f40e5d01bfec0d20
gertahoxha1 <hoxha_gerta14@hotmail.com>
2024-06-23 21:19:27 +0200

    squares r set up, tjerat hala in progress

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ let pickedColor;
 
 let squares = document.querySelectorAll(".square");
 let cDisplay = document.querySelector("#color-display");
+let p = document.querySelector("#head");
 let messageDisplay = document.querySelector("#message");
 let resetButton = document.querySelector("#reset");
 
+startGame();
 
 function startGame() {
-    cDisplay.textContent = pickedColor;
 	putUpSquares();
 	reset();
+    cDisplay.textContent = pickedColor;
 }
-startGame();
 
 resetButton.addEventListener("click", function() {
 	reset();
@@ -22,7 +23,6 @@ resetButton.addEventListener("click", function() {
 function putUpSquares() {
 	for (let i = 0; i < squares.length; i++) {
 		squares[i].style.backgroundColor = colors[i];
-
 		squares[i].addEventListener("click", function() {
 			let clickedColor = this.style.backgroundColor;
 			if(clickedColor === pickedColor) {
@@ -31,10 +31,55 @@ function putUpSquares() {
 				changeColors(pickedColor);
 			}
 			else {
-				this.style.backgroundColor = "#00000";
+				this.style.backgroundColor = "#F7418F";
 				messageDisplay.textContent = "try again";
 			}
 		});
 	}
 }
 
+function reset() {
+	colors = generateColor(numbSquares);
+	pickedColor = chooseColor();
+	p.style.backgroundColor = "#FFFF";
+	cDisplay.textContent = pickedColor;
+	resetButton.textContent = "New Colors";
+	messageDisplay.textContent = "";
+	for (let i = 0; i < squares.length; i++) {
+		if(colors[i]) { 
+			squares[i].style.display = "block";
+			squares[i].style.backgroundColor = colors[i];
+		}
+		else {
+			squares[i].style.display = "none";
+		}
+	}
+}
+
+function changeColors(color) {
+	for(let i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = color;
+		container.style.backgroundColor = color;
+		p.style.backgroundColor = color;
+	}
+}
+
+function chooseColor() {
+	let random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateColor(num) {
+	let array = [];
+	for (let i = 0; i < num; i++) {
+		array.push(makeHexColor());
+	}
+	return array;
+}
+
+function makeHexColor() {
+    let h = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+    let e = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+    let x = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+    return "#" + h + e + x;
+}
